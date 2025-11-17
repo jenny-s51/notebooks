cac3bb9263ac391b739b5fd7032a438b3175f132
Jenny <32821331+jenny-s51@users.noreply.github.com>
2025-11-17 14:46:46 -0500

    feat: implemet create new secret modal
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>
    
    chore: bump to latest 1.2.1 of shared library
    
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>
    
    remove unused div and inline styles
    
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>
    
    fix: add type dropdown, remove console stmts
    
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>
    
    PR feedback, add secret to table when it is created via modal, remove indentation for key/value fields, added helper text to name input
    
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>
    
    fix test by regenerating files
    
    Signed-off-by: Jenny <32821331+jenny-s51@users.noreply.github.com>

--- workspaces/frontend/src/generated/data-contracts.ts
+++ workspaces/frontend/src/generated/data-contracts.ts
@@ -174,13 +174,6 @@ export interface CommonAudit {
   updatedBy: string;
 }
 
-export interface CommonAudit {
-  createdAt: string;
-  createdBy: string;
-  updatedAt: string;
-  updatedBy: string;
-}
-
 export interface HealthCheckHealthCheck {
   status: HealthCheckServiceStatus;
   systemInfo: HealthCheckSystemInfo;
